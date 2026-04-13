04b25e44e666c63ca7c87a31de02379af494faff
PES1UG24AM013 <abhishekph2006@gmail.com>
2026-04-13 14:58:22 +0530

    arp_handler.py

--- arp_handler.py
+++ arp_handler.py
@@ -1,39 +1,65 @@
 from pox.core import core
+import pox.openflow.libopenflow_01 as of
 import pox.lib.packet as pkt
-import pox.openflow.libopenflow_01 as of 
-from pox.lib.addresses import EthAddr
 
 log = core.getLogger()
+
+
 arp_table = {}
 
-def _handle_PacketIn (event):
+def _handle_PacketIn(event):
     packet = event.parsed
+
+    # 🔹 1. Intercept ARP packets
     if packet.type == packet.ARP_TYPE:
         arp_pkt = packet.payload
-        arp_table[arp_pkt.protosrc] = arp_pkt.hwsrc
-        log.info("Learned: %s at %s", arp_pkt.protosrc, arp_pkt.hwsrc)
+        log.info("ARP packet intercepted")
 
+        
+        arp_table[arp_pkt.protosrc] = arp_pkt.hwsrc
+        log.info("Learned Host: %s -> %s", arp_pkt.protosrc, arp_pkt.hwsrc)
+
+
+        
         if arp_pkt.protodst in arp_table:
-            log.info("Proxying ARP reply for %s", arp_pkt.protodst)
-            
-            res = pkt.arp()
-            res.hwsrc = arp_table[arp_pkt.protodst]
-            res.hwdst = arp_pkt.hwsrc
-            res.opcode = pkt.arp.REPLY
-            res.protosrc = arp_pkt.protodst
-            res.protodst = arp_pkt.protosrc
+
+            log.info("Generating ARP reply for %s", arp_pkt.protodst)
+
+            reply = pkt.arp()
+            reply.opcode = pkt.arp.REPLY
+            reply.hwsrc = arp_table[arp_pkt.protodst]
+            reply.hwdst = arp_pkt.hwsrc
+            reply.protosrc = arp_pkt.protodst
+            reply.protodst = arp_pkt.protosrc
 
             eth = pkt.ethernet()
             eth.type = pkt.ethernet.ARP_TYPE
-            eth.src = arp_table[arp_pkt.protodst]
-            eth.dst = arp_pkt.hwsrc
-            eth.set_payload(res)
+            eth.src = reply.hwsrc
+            eth.dst = reply.hwdst
+            eth.payload = reply
 
             msg = of.ofp_packet_out()
             msg.data = eth.pack()
-            msg.actions.append(of.ofp_action_output(port = event.port))
+            msg.actions.append(of.ofp_action_output(port=event.port))
             event.connection.send(msg)
 
-def launch ():
+            log.info("ARP reply sent")
+
+        else:
+            
+            msg = of.ofp_packet_out()
+            msg.data = event.ofp
+            msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+            event.connection.send(msg)
+
+    else:
+        
+        msg = of.ofp_packet_out()
+        msg.data = event.ofp
+        msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        event.connection.send(msg)
+
+
+def launch():
     core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
-    log.info("ARP Proxy Controller is active.")
+    log.info("ARP SDN Controller Started")
